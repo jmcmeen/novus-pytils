a5dbe3e17d36a25f3e3f0b989ef957d70c1a7f6f
John McMeen <johnmcmeen@gmail.com>
2024-07-28 16:23:51 -0400

    updates

--- src/novus_pytils/files.py
+++ src/novus_pytils/files.py
@@ -6,44 +6,46 @@ def get_file_list(directory):
     for root, _, files in os.walk(directory):
         for file in files:
             files.append(os.path.join(root, file))
-
     return file_list
 
 def get_dir_list(directory, relative=False):
     dir_list = []
-    for root, dirs, files in os.walk(directory):
+    for root, dirs, _ in os.walk(directory):
         for dir in dirs:
             if relative:
-                dir_list.append(dir)
+                relative_root = root.replace(directory, '')
+                dir_list.append(os.path.join(relative_root, dir))
             else:
                 dir_list.append(os.path.join(root, dir))
-
     return dir_list
 
 
 def get_files_by_extension(directory, extensions, relative=False):
     file_list = []
-    
-    for root, dirs, files in os.walk(directory):
+    for root, _, files in os.walk(directory):
         for file in files:
-            # Get the file extension
             file_ext = os.path.splitext(file)[1].lower()
 
             for ext in extensions:
                 if ext.casefold() == file_ext.casefold():
                     if relative:
-                        file_list.append(file)
+                        relative_root = root.replace(directory, '')
+                        file_list.append(os.path.join(relative_root, file))
                     else:
                         file_list.append(os.path.join(root, file))
 
     return file_list
 
-def get_files_containing_string(directory, string):
+def get_files_containing_string(directory, string, relative=False):
     file_list = []
-    for root, dirs, files in os.walk(directory):
+    for root, _, files in os.walk(directory):
         for file in files:
             if string.casefold() in file.casefold():
-                file_list.append(file)
+                if relative:
+                    relative_root = root.replace(directory, '')
+                    file_list.append(os.path.join(relative_root, file))
+                else:
+                    file_list.append(os.path.join(root, file))
 
     return file_list
 
@@ -54,25 +56,15 @@ def get_dirs_containing_string(directory, string, relative=False):
         for dir in dirs:
             if string.casefold() in dir.casefold():
                 if relative:
-                    dir_list.append(dir)
+                    relative_root = root.replace(directory, '')
+                    dir_list.append(os.path.join(relative_root, dir))
                 else:
                     dir_list.append(os.path.join(root, dir))
 
     return dir_list
 
-def write_list_to_file(file_name, lines):
-    with open(file_name, 'w') as file:
-        for line in lines:
-            file.write(line + '\n')
-
-def read_list_from_file(file_name):
-    with open(file_name, 'r') as file:
-        lines = file.readlines()
-    return [line.strip() for line in lines]
-
-
 def directory_contains_directory(directory, subdirectory):
-    for root, dirs, files in os.walk(directory):
+    for _, dirs, _ in os.walk(directory):
         for dir in dirs:
             if subdirectory.casefold() in dir.casefold():
                 return True
@@ -86,7 +78,7 @@ def directory_contains_file(directory, filename):
     return False
 
 def directory_contains_file_with_extension(directory, extension):
-    for root, dirs, files in os.walk(directory):
+    for _, _, files in os.walk(directory):
         for file in files:
             if extension.casefold() in os.path.splitext(file)[1].casefold():
                 return True
@@ -102,6 +94,9 @@ def create_subdirectory(parent_dir, subdirectory_name):
 def copy_file(src_file_path, dest_file_path):
     shutil.copy2(src_file_path, dest_file_path)
 
+def move_file(src_file_path, dest_file_path):
+    shutil.move(src_file_path, dest_file_path)
+
 def directory_exists(directory_path):
     return os.path.exists(directory_path) and os.path.isdir(directory_path)
 
@@ -109,13 +104,13 @@ def delete_directory(directory_path):
     if os.path.exists(directory_path):
         if os.path.isdir(directory_path):
             shutil.rmtree(directory_path)
-        else:
-            print("The path is not a directory!")
-    else:
-        print("The directory does not exist!")
 
 def get_file_extension(file_path):
     return os.path.splitext(file_path)[1].lower()
 
 def get_file_name(file_path):
     return os.path.basename(file_path)
+
+def get_file_directory(file_path):
+    return os.path.dirname(file_path)
+
--- src/novus_pytils/lists.py
+++ src/novus_pytils/lists.py
@@ -5,3 +5,13 @@ def remove_empty_lines_from_list(list):
 def print_list(list):
     for item in list:
         print(item)
+
+def write_list_to_file(file_name, lines):
+    with open(file_name, 'w') as file:
+        for line in lines:
+            file.write(line + '\n')
+
+def read_list_from_file(file_name):
+    with open(file_name, 'r') as file:
+        lines = file.readlines()
+    return [line.strip() for line in lines]
