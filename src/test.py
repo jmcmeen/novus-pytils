0b9e4b51d179937011172c0d58059200e71efcc0
John McMeen <johnmcmeen@gmail.com>
2024-07-30 20:03:39 -0400

    add some new things

--- src/test.py
+++
@@ -1,5 +0,0 @@
-# not an official test package
-
-from novus_pytils import files, wav
-for file in files.get_files_by_extension("/media/john/The Archive/bioamla_data", [".wav"]):
-    print(wav.read_wav_file_metadata(file))
