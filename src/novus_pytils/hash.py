e0a9055a3a1605f0fe268d24068ede72de7392c5
John McMeen <johnmcmeen@gmail.com>
2025-06-27 03:48:01 -0400

    Update hash.py

--- src/novus_pytils/hash.py
+++ src/novus_pytils/hash.py
@@ -1,4 +1,5 @@
 import hashlib
+
 def get_file_md5_hash(file_path):
     """
     Calculate the MD5 hash of a file.
@@ -9,35 +10,12 @@ def get_file_md5_hash(file_path):
     Returns:
         str: The MD5 hash of the file in hexadecimal format.
     """
-
-    with open(file_path, 'rb') as file:
-        md5_hash = hashlib.md5(file.read()).hexdigest()
-    return md5_hash
-
-def get_str_md5_hash(string):
-    """
-    Calculate the MD5 hash of a given string.
-
-    Args:
-        string (str): The input string to hash.
-
-    Returns:
-        str: The MD5 hash of the string in hexadecimal format.
-    """
-
-    return hashlib.md5(string.encode()).hexdigest()
-
-def get_str_sha256_hash(string):
-    """
-    Calculate the SHA-256 hash of a given string.
-
-    Args:
-        string (str): The input string to hash.
-
-    Returns:
-        str: The SHA-256 hash of the string in hexadecimal format.
-    """
-    return hashlib.sha256(string.encode()).hexdigest()
+    hash_md5 = hashlib.md5()
+    with open(file_path, "rb") as f:
+        # Read file in chunks to handle large files efficiently
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
 
 def get_file_sha256_hash(file_path):
     """
@@ -49,8 +27,9 @@ def get_file_sha256_hash(file_path):
     Returns:
         str: The SHA-256 hash of the file in hexadecimal format.
     """
-
-    with open(file_path, 'rb') as file:
-        sha256_hash = hashlib.sha256(file.read()).hexdigest()
-    return sha256_hash
-
+    hash_sha256 = hashlib.sha256()
+    with open(file_path, "rb") as f:
+        # Read file in chunks to handle large files efficiently
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_sha256.update(chunk)
+    return hash_sha256.hexdigest()
