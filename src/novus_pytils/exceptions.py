2c9f61a3eab6461895ae774cc88d299b3133cc0f
John McMeen <johnmcmeen@gmail.com>
2025-06-26 21:47:18 -0400

    v 0.0.39

--- src/novus_pytils/exceptions.py
+++ src/novus_pytils/exceptions.py
@@ -2,3 +2,6 @@ class PathNotFoundException(Exception):
     def __init__(self, invalid_value):
         super().__init__("Path not found")
         self.invalid_value = invalid_value
+
+    def __str__(self):
+        return f"Path not found: {self.invalid_value}"
