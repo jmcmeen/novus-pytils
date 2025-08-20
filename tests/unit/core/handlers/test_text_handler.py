bc536cbf5961defc5411d78767db28bbff8a062b
John McMeen <johnmcmeen@gmail.com>
2025-08-19 22:46:58 -0400

    reorganization

--- tests/unit/core/handlers/test_text_handler.py
+++ tests/unit/core/handlers/test_text_handler.py
@@ -2,7 +2,7 @@
 import pytest
 from unittest.mock import patch, mock_open
 
-from novus_pytils.models.handlers.text_handler import TextHandler
+from novus_pytils.core.handlers.text_handler import TextHandler
 from novus_pytils.core.exceptions import FileHandlerError, UnsupportedFormatError
 
 class TestTextHandler:
